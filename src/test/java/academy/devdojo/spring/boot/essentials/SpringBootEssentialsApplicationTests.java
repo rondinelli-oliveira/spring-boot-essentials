a75c05dad1f0ce62a78264d0da596d0418f2c2b8
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-02-09 13:55:10 -0300

    Project created

---
+++ src/test/java/academy/devdojo/spring/boot/essentials/SpringBootEssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.devdojo.spring.boot.essentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
