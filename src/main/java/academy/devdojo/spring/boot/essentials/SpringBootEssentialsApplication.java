f53a7a86ae8feb4b07fac19ce654db6441a293df
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-02-10 11:00:09 -0300

    aula-04 - @Component, @Autowired, @SpringBootApplication

--- src/main/java/academy/devdojo/spring/boot/essentials/SpringBootEssentialsApplication.java
+++ src/main/java/academy/devdojo/spring/boot/essentials/SpringBootEssentialsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootEssentialsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootEssentialsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEssentialsApplication.class, args);
+    }
 
 }
