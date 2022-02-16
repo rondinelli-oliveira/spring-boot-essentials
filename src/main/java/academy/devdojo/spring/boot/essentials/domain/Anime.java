d6b31e815467457372519ec32390164d9b5ee0ef
rondinelli-oliveira <rondinelliads@gmail.com>
2022-02-15 23:29:38 -0300

    aula-13 - Spring Data JPA pt 01

--- src/main/java/academy/devdojo/spring/boot/essentials/domain/Anime.java
+++ src/main/java/academy/devdojo/spring/boot/essentials/domain/Anime.java
@@ -3,10 +3,17 @@ package academy.devdojo.spring.boot.essentials.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
 @AllArgsConstructor
+@Entity
 public class Anime {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
