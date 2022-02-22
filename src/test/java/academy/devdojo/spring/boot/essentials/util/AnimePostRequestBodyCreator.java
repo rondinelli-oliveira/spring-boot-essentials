94d8a89c1919ac3124c3d69cb2cc0d923d88fbbb
Rondinelli Oliveira <rondinellisilva.act@sulamerica.com.br>
2022-02-22 14:09:33 -0300

    aula-34 - Unit Tests pt 02

---
+++ src/test/java/academy/devdojo/spring/boot/essentials/util/AnimePostRequestBodyCreator.java
@@ -0,0 +1,12 @@
+package academy.devdojo.spring.boot.essentials.util;
+
+import academy.devdojo.spring.boot.essentials.requests.AnimePostRequestBody;
+
+public class AnimePostRequestBodyCreator {
+
+    public static AnimePostRequestBody createAnimePostRequestBody() {
+        return AnimePostRequestBody.builder()
+                .name(AnimeCreator.createAnimeToBeSaved().getName())
+                .build();
+    }
+}
